f56ca09153cc0972877add0b542024dd1b2f4402
unknown <1831699658qaz123@gmail.com>
2022-07-12 11:46:46 +0800

    update

--- public/app.js
+++ public/app.js
@@ -34,7 +34,39 @@
     };
 
     /* 從Api取得JSON數據 */
-    myConnector.getData = function (table, doneCallback) {};
+    myConnector.getData = function (table, doneCallback) {
+        let tableDate = []
+        var i = 0
 
+        // 用jquery Api request 
+        $.getJSON("https://take2healthdataextractionapi.herokuapp.com/dataextraction", function(resp) {
+            for (i = 0, len = resp.length; i < len; i++) {
+                tableData.push({
+                    "batch_id": resp[i].batch_id,
+                    "id": resp[i].properties.id,
+                    "Operation": resp[i].properties.Operation,
+                });
+            }
+            table.appendRows(tableDate);
+            doneCallback();
+        });
+
+    };
+
+
+    /* 註冊Connector 物件 */  
     tableau.registerConnector(myConnector);
 })();
+
+/* 將Connector物件與Submit按鈕勾取(hook up)在一起
+    當Submit按鈕被點擊時，調用getData function*/
+document.querySelector("#submitButton").addEventListener('click', getData)
+
+/* getData function
+    初始化Connector 物件，並連接tableau desktop*/
+function getData() {
+    // 連接器在tableau裏的名字
+    tableau.connectionName = "USGS Earthquake Feed";
+    tableau.submit()
+    
+}
