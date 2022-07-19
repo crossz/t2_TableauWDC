400ded6b2d6bf7891e1c4f3dd267982ee7e89d27
unknown <1831699658qaz123@gmail.com>
2022-07-19 08:52:23 +0800

    update

--- public/app.js
+++ public/app.js
@@ -1,63 +1,67 @@
 (function() {
-    // Create the connector object
-    var myConnector = tableau.makeConnector();
-
-    // Define the schema
+    const optionElement = document.getElementById("dropDownListForApi");
+    optionElement.addEventListener("click",function(){
     
-    myConnector.getSchema = function(schemaCallback) {
-        var cols = [{
-            id: "Master_Lab_ID",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "approval_time",
-            dataType: tableau.dataTypeEnum.datetime
-        }, {
-            id: "is_qPCR",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "is_repeat",
-            dataType: tableau.dataTypeEnum.string
-        }];
+        // Create the connector object
+        var myConnector = tableau.makeConnector();
 
-        var tableSchema = {
-            id: "qPCRRepeatCaseSchema",
-            alias: "Schemafor qPCR Repeat Case Dashboard",
-            columns: cols
+        // Define the schema
+        
+        myConnector.getSchema = function(schemaCallback) {
+            var cols = [{
+                id: "Master_Lab_ID",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "approval_time",
+                dataType: tableau.dataTypeEnum.datetime
+            }, {
+                id: "is_qPCR",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "is_repeat",
+                dataType: tableau.dataTypeEnum.string
+            }];
+
+            var tableSchema = {
+                id: "qPCRRepeatCaseSchema",
+                alias: "Schemafor qPCR Repeat Case Dashboard",
+                columns: cols
+            };
+
+            schemaCallback([tableSchema]);
         };
 
-        schemaCallback([tableSchema]);
-    };
-
-  
-    // Download the data
-    myConnector.getData = function(table, doneCallback) {
-
-        $.getJSON("https://take2healthdataextractionapi.herokuapp.com/dataextraction", function(resp) {
-            var dataSourceTable = resp.table,
-                tableData = [];
-            
-            // Iterate over the JSON object
-            for (var i = 0 ; i < dataSourceTable.length; i++) {
-                tableData.push({
-                    "Master_Lab_ID": dataSourceTable[i]["Master_Lab_ID"],
-                    "approval_time": dataSourceTable[i]["approval_time"],
-                    "is_qPCR": dataSourceTable[i]["is_qPCR"],
-                    "is_repeat": dataSourceTable[i]["is_repeat"]
-                });
-            }
-
-            table.appendRows(tableData);
-            doneCallback();
-        });
-    };
-    tableau.registerConnector(myConnector);
-
     
-    $(document).ready(function() {
-        $("#submitButton").click(function() {
-            console.log("It is working!");
-            tableau.connectionName = "qPCRRepeatCase"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
+        // Download the data
+        myConnector.getData = function(table, doneCallback) {
+
+            $.getJSON("https://take2healthdataextractionapi.herokuapp.com/dataextraction", function(resp) {
+                var dataSourceTable = resp.table,
+                    tableData = [];
+                
+                // Iterate over the JSON object
+                for (var i = 0 ; i < dataSourceTable.length; i++) {
+                    tableData.push({
+                        "Master_Lab_ID": dataSourceTable[i]["Master_Lab_ID"],
+                        "approval_time": dataSourceTable[i]["approval_time"],
+                        "is_qPCR": dataSourceTable[i]["is_qPCR"],
+                        "is_repeat": dataSourceTable[i]["is_repeat"]
+                    });
+                }
+
+                table.appendRows(tableData);
+                doneCallback();
+            });
+        };
+        tableau.registerConnector(myConnector);
+
+        
+        $(document).ready(function() {
+            $("#submitButton").click(function() {
+                console.log("It is working!");
+                tableau.connectionName = "qPCRRepeatCase"; // This will be the data source name in Tableau
+                tableau.submit(); // This sends the connector object to Tableau
+            });
         });
     });
 })();
